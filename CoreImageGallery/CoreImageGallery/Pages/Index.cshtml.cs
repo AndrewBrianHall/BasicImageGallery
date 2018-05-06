5702a477ad2430187cecaddd3d72ba799d81bd72
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2018-05-05 23:59:49 -0700

    Adding stub for Cosmos

--- CoreImageGallery/CoreImageGallery/Pages/Index.cshtml.cs
+++ CoreImageGallery/CoreImageGallery/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using CoreImageGallery.Models;
 using CoreImageGallery.Services;
@@ -37,12 +38,20 @@ namespace CoreImageGallery.Pages
         public async Task<IActionResult> OnPostAsync(IFormFile file)
         {
             var fileName = Path.GetFileName(file.FileName);
-            await _storageService.AddImageAsync(file.OpenReadStream(), fileName);
-
             DateTime now = DateTime.Now;
             var base64Time = Base64Encoder.GetBase64String($"{now.ToShortDateString()} {now.ToShortTimeString()}");
             var base64File = Base64Encoder.GetBase64String(fileName);
+            var user = User.Identities.FirstOrDefault();
+
+            await _storageService.AddImageAsync(file.OpenReadStream(), fileName);
+            await RecordUploadAsync(user.Name, fileName, now);
+
             return RedirectToPage("UploadSuccess", new { file = base64File, time = base64Time });
+        }
+
+        public async Task RecordUploadAsync(string userId, string file, DateTime timeStamp)
+        {
+
         }
     }
 }
