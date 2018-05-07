abfcd9f996d3cf9b92bd4b27b85c5accbc67e8f9
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2018-05-07 16:45:03 -0700

    First steps to adding cosmos function integration

--- CoreImageGallery/ImageGallery.AzFunctions/GalleryImageUploaded.cs
+++ CoreImageGallery/ImageGallery.AzFunctions/GalleryImageUploaded.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using ImageGallery.Model;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -8,18 +9,19 @@ namespace Watermarker
     public static class GalleryImageUploaded
     {
         const string WatermarkMessage = "CoreImageGallery";
-        
+
         [FunctionName("Watermarker")]
         public static void Run([BlobTrigger("images/{name}")]Stream inputBlob,
                                [Blob("images-watermarked/{name}", FileAccess.Write)] Stream outputBlob,
-                               string name, 
+                               [DocumentDB("pyimagegallerydb", "", ConnectionStringSetting = "CosmosDBConnection"] Image image,
+                               string name,
                                TraceWriter log)
         {
-            log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {inputBlob.Length} Bytes");
 
             try
             {
                 ImageMarker.WriteWatermark(WatermarkMessage, inputBlob, outputBlob);
+                log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {inputBlob.Length} Bytes");
             }
             catch (Exception e)
             {
---
+++ CoreImageGallery/ImageGallery.Model/Image.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ImageGallery.Model
+{
+    public class Image
+    {
+        public string FileName { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
