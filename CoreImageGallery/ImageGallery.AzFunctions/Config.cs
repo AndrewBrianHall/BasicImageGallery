dbf102e00126a5cdbde5583e36a4f0f63cc6e95f
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2018-05-07 16:01:58 -0700

    Adding delete image

---
+++ CoreImageGallery/ImageGallery.AzFunctions/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageGallery.AzFunctions
+{
+    class Config
+    {
+        public const string UploadContainer = "images";
+        public const string WatermarkedContainer = "images-watermarked";
+        public const string StorageConnectionStringName = "AzureWebJobsStorage";
+    }
+}
