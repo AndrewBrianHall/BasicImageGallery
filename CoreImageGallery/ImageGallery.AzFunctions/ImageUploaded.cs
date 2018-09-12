1ecd992d92b822dcca6a7a8402a0392aaf0fbadd
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2018-09-12 15:56:42 -0700

    updating functions project for v2 changes

--- CoreImageGallery/ImageGallery.AzFunctions/ImageUploaded.cs
+++ CoreImageGallery/ImageGallery.AzFunctions/ImageUploaded.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using ImageGallery.Model;
-using ImageGallery.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Azure.ApiHub;
 
 namespace Watermarker
 {
