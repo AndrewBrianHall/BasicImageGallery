0df725a54d678a46ac03316b596d0417cb194a37
Paul Yuknewicz <paulyuk@microsoft.com>
2018-12-04 15:56:35 -0800

    Added ILogger and basic logging for Index page.

--- CoreImageGallery/CoreImageGallery/Pages/Index.cshtml.cs
+++ CoreImageGallery/CoreImageGallery/Pages/Index.cshtml.cs
@@ -9,26 +9,40 @@ using ImageGallery.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using Watermarker;
 
 namespace CoreImageGallery.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger _logger;
         private const string UploadSuccessParameter = "uploadSuccess";
         public IEnumerable<UploadedImage> Images { get; set; }
         public bool UploadSuccess { get; set; } = false;
 
         private IStorageService _storageService;
 
-        public IndexModel(IStorageService storageService)
+        public IndexModel(IStorageService storageService, ILogger<IndexModel> logger)
         {
             _storageService = storageService;
+            _logger = logger;
+            Images = new List<UploadedImage>();
         }
 
         public async Task OnGetAsync(string value)
         {
-            this.Images = await _storageService.GetImagesAsync();
+            _logger.LogInformation("Getting images from storage - started.");
+            try
+            {
+                this.Images = await _storageService.GetImagesAsync();
+                _logger.LogInformation("Getting images from storage - complete.");
+                _logger.LogInformation($"Getting images from storage - {this.Images.Count()}.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Getting images from storage - error: {ex.ToString()}");
+            }
 
             if (value == UploadSuccessParameter)
             {
@@ -38,18 +52,22 @@ namespace CoreImageGallery.Pages
 
         public async Task<IActionResult> OnPostAsync(IFormFile file)
         {
+            _logger.LogInformation($"Uploading {file.FileName} to storage - started.");
             string fileName = Path.GetFileName(file.FileName);
             DateTime now = DateTime.Now;
             string base64Time = Base64Encoder.GetBase64String($"{now.ToShortDateString()} {now.ToShortTimeString()}");
             string base64File = Base64Encoder.GetBase64String(fileName);
             ClaimsIdentity user = this.User.Identities.FirstOrDefault();
 
+            _logger.LogInformation($"Watermarking {file.FileName} - started.");
             MemoryStream watermarkedImage = new MemoryStream();
             Stream originalImageStrm = file.OpenReadStream();
             WaterMarker.WriteWatermark(originalImageStrm, watermarkedImage, user.Name);
+            _logger.LogInformation($"Watermarking {file.FileName} - completed.");
 
             await _storageService.AddImageAsync(watermarkedImage, fileName, user.Name);
 
+            _logger.LogInformation($"Uploading {file.FileName} to storage - completed.");
             return RedirectToPage("UploadSuccess", new { file = base64File, time = base64Time });
         }
 
--- CoreImageGallery/CoreImageGallery/Program.cs
+++ CoreImageGallery/CoreImageGallery/Program.cs
@@ -14,12 +14,20 @@ namespace CoreImageGallery
     {
         public static void Main(string[] args)
         {
+
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    //logging.AddEventSourceLogger();
+                })
                 .Build();
     }
 }
