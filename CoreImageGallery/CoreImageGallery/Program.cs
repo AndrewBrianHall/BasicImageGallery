bde07d59e784d73b46a3aba873214b666ee9aa8e
Paul Yuknewicz <paulyuk@microsoft.com>
2019-09-19 02:07:49 -0700

    Add Application Insights in SDK mode.  For cloud environment to work you must set the APPINSIGHTS_INSTRUMENTATIONKEY environment variable.

--- CoreImageGallery/CoreImageGallery/Program.cs
+++ CoreImageGallery/CoreImageGallery/Program.cs
@@ -19,6 +19,7 @@ namespace CoreImageGallery
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
